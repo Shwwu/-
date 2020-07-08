e984e00bb385be6cfaa607b4472f9db468e9646e
shwwu <limitss2010@gmail.com>
2020-07-09 03:56:30 +0800

    準備測式的資料

---
+++ src/nba/com/testdd.java
@@ -0,0 +1,22 @@
+package nba.com;
+
+public class testdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello git .......");
+		System.out.println("hello git .......");
+		System.out.println("hello git .......");
+		System.out.println("hello git .......");
+		System.out.println("hello git .......");
+		System.out.println("hello git .......");
+		System.out.println("hello git .......");
+		System.out.println("hello git .......");
+		System.out.println("hello git .......");
+		System.out.println("hello git .......");
+		System.out.println("hello git .......");
+		 
+	}
+
+}
